242175d8300369798e0ec2fa2c0825639560f6d6
Luka Stojiljkovic <luka.d.stojiljkovic@gmail.com>
2026-05-18 14:11:08 +0200

    build: prazan banka2-contracts modul

---
+++ banka2-contracts/src/main/java/rs/raf/banka2/contracts/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Deljeni inter-servisni ugovori: DTO-i, enumi, event payload-i,
+ * request/response record-i internog API-ja. Bez logike, bez JPA entiteta.
+ * Faza 0: prazan modul - popunjava se u Fazi 1+ kad krene inter-servisna komunikacija.
+ */
+package rs.raf.banka2.contracts;
